985245f17feff7d775f294467e9ed76a6efb4c58
aonemd <aonemdsaleh@gmail.com>
2019-09-30 02:37:35 +0200

    Add [given] sample test case to MainTest

--- test/MainTest.java
+++ test/MainTest.java
@@ -6,6 +6,12 @@ import org.junit.Test;
 public class MainTest {
     @Test
     public void testHelloWorld() {
-        assertEquals(1, 1);
+
+        String solution = Main.solve("5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3",
+                "BF",
+                false);
+
+        assertEquals("up,collect,left,down,collect,down,collect,right,collect,kill,down,down,left,collect,left,collect,right,up,snap;63;52333",
+                solution);
     }
 }
