65a1e8d184beea8d48145174c4d626cf3eedab96
aonemd <aonemdsaleh@gmail.com>
2019-10-07 00:02:02 +0200

    Move grid parsing into the constructor of `EndGame`

--- src/EndGame.java
+++ src/EndGame.java
@@ -3,35 +3,48 @@ package endgame;
 import java.util.ArrayList;
 
 public class EndGame {
-    public static String solve(String grid, String strategy, boolean visualize) {
+    private String grid;
+    private int gridWidth;
+    private int gridHeight;
+    private Cell ironManPosition;
+    private Cell thanosPosition;
+    private ArrayList<Cell> stonePositions;
+    private ArrayList<Cell> warriorPositions;
+
+    public EndGame(String grid) {
         // parse grid into useful information
         String[] gridInfo       = grid.split(";");
 
         String[] gridDimensions = gridInfo[0].split(",");
-        String gridWidth        = gridDimensions[0];
-        String gridHeight       = gridDimensions[1];
+        this.gridWidth = Integer.parseInt(gridDimensions[0]);
+        this.gridHeight = Integer.parseInt(gridDimensions[1]);
 
         String[] ironManPosition = gridInfo[1].split(",");
-        String ironManX          = ironManPosition[0];
-        String ironManY          = ironManPosition[1];
+        this.ironManPosition = new Cell(Integer.parseInt(ironManPosition[0]),
+                Integer.parseInt(ironManPosition[1]));
 
         String[] thanosPosition = gridInfo[2].split(",");
-        String thanosX          = thanosPosition[0];
-        String thanosY          = thanosPosition[1];
+        this.thanosPosition = new Cell(Integer.parseInt(thanosPosition[0]),
+                Integer.parseInt(thanosPosition[1]));
 
-        ArrayList<String[]> stonePositions = new ArrayList<String[]>();
+        this.stonePositions = new ArrayList<Cell>();
         String[] stoneXYPositions = gridInfo[3].split(",");
         for(int i = 0; i < stoneXYPositions.length; i += 2) {
-            String[] stonePosition = {stoneXYPositions[i], stoneXYPositions[i+1]};
-            stonePositions.add(stonePosition);
+            this.stonePositions.add(new Cell(Integer.parseInt(stoneXYPositions[i]),
+                        Integer.parseInt(stoneXYPositions[i+1])));
         }
 
-        ArrayList<String[]> warriorPositions = new ArrayList<String[]>();
+        this.warriorPositions = new ArrayList<Cell>();
         String[] warriorXYPositions = gridInfo[4].split(",");
         for(int i = 0; i < warriorXYPositions.length; i += 2) {
-            String[] warriorPosition = {warriorXYPositions[i], warriorXYPositions[i+1]};
-            warriorPositions.add(warriorPosition);
+            this.warriorPositions.add(new Cell(Integer.parseInt(warriorXYPositions[i]),
+                        Integer.parseInt(warriorXYPositions[i+1])));
         }
+
+    }
+
+    public static String solve(String grid, String strategy, boolean visualize) {
+        EndGame endGameProblem = new EndGame(grid);
 
         return search(grid, strategy);
     }
