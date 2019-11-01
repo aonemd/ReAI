e2fbc9add6f98f84d0ccb2aecd1ce81618997877
aonemd <aonemdsaleh@gmail.com>
2019-11-01 20:34:05 +0200

    Add two simpler test cases for EndGame `solve()`

--- test/EndGameTest.java
+++ test/EndGameTest.java
@@ -5,6 +5,20 @@ import org.junit.Test;
 
 public class EndGameTest {
     @Test
+    public void testFinalCell() {
+        String solution = EndGame.solve("2,2;0,0;0,0", "BF", false);
+
+        assertEquals("snap", solution);
+    }
+
+    @Test
+    public void testOneCellBeforeFinal() {
+        String solution = EndGame.solve("2,2;0,0;0,1", "BF", false);
+
+        assertEquals("down,snap", solution);
+    }
+
+    @Test
     public void testHelloWorld() {
 
         String solution = EndGame.solve("5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3",
