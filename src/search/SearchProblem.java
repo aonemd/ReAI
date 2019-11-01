47666917b96b6ce39b1d95b6a8711d02c6a86e6d
aonemd <aonemdsaleh@gmail.com>
2019-11-01 20:42:14 +0200

    Implement BFS for SearchProblem general `search()`

--- src/search/SearchProblem.java
+++ src/search/SearchProblem.java
@@ -1,22 +1,38 @@
 package search;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import static util.Inspector.inspect;
 
 public abstract class SearchProblem {
     public HashMap<String, Operator> operators;
     public State initialState;
 
-    // General Search
     public static String search(SearchProblem searchProblem, String strategy) {
-        // convert initial state into a node
-        // add this node to nodes list
-        // loop over nodes
-        //   node = removeFront(nodes)
-        //   return node if node is goals <3
-        //   nodes << node.extend()
-        // end loop
+        List<SearchTreeNode> nodeList = new SearchTreeNode(searchProblem.initialState, null, null, 0, 0).expand();
 
-        return "";
+        List<State> visitedStates = new ArrayList<State>();
+
+        SearchTreeNode currentNode = null;
+        Iterator<SearchTreeNode> nodeListIterator = nodeList.iterator();
+        while (nodeListIterator.hasNext()) {
+            currentNode = nodeList.remove(0);
+
+            if (currentNode.state.isGoal()) {
+                return currentNode.toPlan();
+            } else {
+                if (visitedStates.contains(currentNode.state)) {
+                    continue;
+                }
+
+                nodeList.addAll(currentNode.expand());
+            }
+
+            visitedStates.add(currentNode.state);
+        }
+
+        return "There is no solution";
     }
-
 }
