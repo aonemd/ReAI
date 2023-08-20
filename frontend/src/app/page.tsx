778fc16b6b4d459fd1fd783eb681633d90ac6695
aonemd <asalehmo7@gmail.com>
2023-08-20 10:36:14 +0200

    Render all states in sequence
    Like a movie

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,47 @@
 
 import { useState, useEffect } from "react";
 
-const GridContainer = ({ children }) => {
-  return <div className="">{children}</div>;
+const Grid = ({ node }) => {
+  let ironMan = node.state.ironManPosition;
+  let thanos = node.state.thanosPosition;
+  let stonePositions = node.state.stonePositions;
+  let warriorPositions = node.state.warriorPositions;
+
+  return (
+    <div className="">
+      {[...Array(5)].map((_, i) => {
+        return (
+          <div className="flex flex-row justify-center items-center">
+            {Array(5)
+              .fill("-")
+              .map((_, j) => {
+                let cellValue;
+                let curCell = { x: i, y: j };
+
+                if (
+                  stonePositions.some(
+                    (pos) => pos.x == curCell.x && pos.y == curCell.y
+                  )
+                )
+                  cellValue = "S";
+                if (
+                  warriorPositions.some(
+                    (pos) => pos.x == curCell.x && pos.y == curCell.y
+                  )
+                )
+                  cellValue = "W";
+                if (thanos.x == curCell.x && thanos.y == curCell.y)
+                  cellValue = "T";
+                if (ironMan.x == curCell.x && ironMan.y == curCell.y)
+                  cellValue = "I";
+
+                return <Cell key={i} value={cellValue} />;
+              })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 const Cell = ({ value }) => {
   return (
@@ -16,6 +55,7 @@ const Cell = ({ value }) => {
 export default function Home() {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
+  const [currentNodeIndex, setCurrentNodeIndex] = useState(0);
 
   useEffect(() => {
     async function callAPI() {
@@ -38,49 +78,18 @@ export default function Home() {
   if (isLoading) return <p>Loading...</p>;
   if (!data) return <p>No profile data</p>;
 
-  let node = data.path[0]!;
-  let ironMan = node.state.ironManPosition;
-  let thanos = node.state.thanosPosition;
-  let stonePositions = node.state.stonePositions;
-  let warriorPositions = node.state.warriorPositions;
+  if (currentNodeIndex < data.path.length - 1) {
+    setTimeout(() => {
+      console.log("Index updated");
+      setCurrentNodeIndex(currentNodeIndex + 1);
+    }, 300);
+  }
 
   return (
     <div>
       <h1>Hell, world!</h1>
 
-      <GridContainer>
-        {[...Array(5)].map((_, i) => {
-          return (
-            <div className="flex flex-row justify-center items-center">
-              {Array(5)
-                .fill("-")
-                .map((_, j) => {
-                  let cellValue;
-                  let curCell = { x: i, y: j };
-
-                  if (
-                    stonePositions.some(
-                      (pos) => pos.x == curCell.x && pos.y == curCell.y
-                    )
-                  )
-                    cellValue = "S";
-                  if (
-                    warriorPositions.some(
-                      (pos) => pos.x == curCell.x && pos.y == curCell.y
-                    )
-                  )
-                    cellValue = "W";
-                  if (thanos.x == curCell.x && thanos.y == curCell.y)
-                    cellValue = "T";
-                  if (ironMan.x == curCell.x && ironMan.y == curCell.y)
-                    cellValue = "I";
-
-                  return <Cell key={i} value={cellValue} />;
-                })}
-            </div>
-          );
-        })}
-      </GridContainer>
+      <Grid key={currentNodeIndex} node={data.path[currentNodeIndex]} />
     </div>
   );
 }
