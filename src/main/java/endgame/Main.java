23295c0cabc9a6bba6a81adf0c75c572ee0e74a8
aonemd <asalehmo7@gmail.com>
2023-08-24 13:01:30 +0200

    Add more test cases

--- src/main/java/endgame/Main.java
+++ src/main/java/endgame/Main.java
@@ -1,21 +1,26 @@
 package endgame;
 
 import search.SearchProblem;
-import search.strategy.BFS;
+import search.strategy.*;
 
 public class Main {
     public static void main(String[] args) {
         SearchProblem problem = new Endgame();
 
         String input = "5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3";
-        // String input = "3,3;0,0;1,0;0,2;2,0";
+        // String input = "2,2;1,0;0,1;";
+        // String input = "3,3;2,0;0,2;1,2;";
+        // String input = "3,3;2,0;0,2;1,2;0,1";
+        // String input = "3,3;0,0;1,0;0,2,2,2;2,0";
         // String input = "3,3;0,0;1,0;0,2,2,1;2,0";
         // String input = "2,2;0,0;1,0;1,1;0,1";
         // String input = "2,2;0,0;1,0;1,1;0,1";
+        // String input = "2,2;1,0;1,1";
+        // String input = "3,3;2,0;2,2";
 
         long startTime = System.nanoTime();
 
-        String solution = problem.solve(input, new BFS(), true);
+        String solution = problem.solve(input, new IDS(), true);
         System.out.println("\n+++\n" + solution);
 
         long endTime = System.nanoTime();
