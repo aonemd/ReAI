475908de5ca5ffe2947c86107841666a14054cfa
aonemd <asalehmo7@gmail.com>
2023-08-24 20:49:50 +0200

    Add a menu to select search algorithm and search btn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -58,25 +58,43 @@ const Cell = ({ value }) => {
 };
 
 export default function Home() {
+  const [strategies, setStrategies] = useState(null);
+  const [algo, setAlgo] = useState("bfs");
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
   const [currentNodeIndex, setCurrentNodeIndex] = useState(0);
 
-  useEffect(() => {
-    async function callAPI() {
-      try {
-        const res = await fetch(
-          `http://localhost:8080/api/endgame/search?grid=5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3&algo=ids`
-        );
-        const data = await res.json();
-        console.log(data);
-        setData(data);
-        setLoading(false);
-      } catch (err) {
-        console.log(err);
-      }
-    }
+  async function getAlgos() {
+    try {
+      const res = await fetch(
+        `http://localhost:8080/api/endgame/search/strategies`
+      );
+      const data = await res.json();
 
+      setStrategies(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function callAPI() {
+    try {
+      const res = await fetch(
+        `http://localhost:8080/api/endgame/search?grid=5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3&algo=${algo}`
+      );
+      const data = await res.json();
+      console.log(data);
+      setData(data);
+      setCurrentNodeIndex(0);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getAlgos();
     callAPI();
   }, []);
 
@@ -106,6 +124,30 @@ export default function Home() {
         <div className="capitalize text-lg">
           expanded nodes: {data.numOfNodes}
         </div>
+      </div>
+
+      <div className="flex flex-row justify-center items-center space-x-4 mt-7">
+        <select
+          className="py-3 px-4 pr-9 bg-transparent border-gray-200 text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+          placeholder="Algo"
+          value={algo}
+          onChange={(event) => setAlgo(event.target.value)}
+        >
+          {strategies.map((algo, i) => {
+            return (
+              <option value={algo} key={i} className="uppercase">
+                {algo}
+              </option>
+            );
+          })}
+        </select>
+
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent"
+          onClick={callAPI}
+        >
+          search
+        </button>
       </div>
     </div>
   );
